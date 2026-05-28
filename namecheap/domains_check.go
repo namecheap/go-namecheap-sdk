a1b0d71e0e2fcd5db5957d0e6a46dae9f74b1aec
Yuriy R <22548029+kurok@users.noreply.github.com>
2026-05-28 14:38:33 +0100

    Introduce domains check method (#69)
    Signed-off-by: yuriyryabikov <22548029+kurok@users.noreply.github.com>
    Co-authored-by: a.o.ivanov <misantron@gmail.com>

---
+++ namecheap/domains_check.go
@@ -0,0 +1,57 @@
+package namecheap
+
+import (
+	"encoding/xml"
+	"fmt"
+	"strings"
+)
+
+type DomainsCheckResponse struct {
+	XMLName *xml.Name `xml:"ApiResponse"`
+	Errors  *[]struct {
+		Message *string `xml:",chardata"`
+		Number  *string `xml:"Number,attr"`
+	} `xml:"Errors>Error"`
+	CommandResponse *DomainsCheckCommandResponse `xml:"CommandResponse"`
+}
+
+type DomainsCheckCommandResponse struct {
+	DomainCheckResults *[]DomainCheckResult `xml:"DomainCheckResult"`
+}
+
+type DomainCheckResult struct {
+	Domain                   *string  `xml:"Domain,attr"`
+	IsAvailable              *bool    `xml:"Available,attr"`
+	IsPremiumName            *bool    `xml:"IsPremiumName,attr"`
+	PremiumRegistrationPrice *float64 `xml:"PremiumRegistrationPrice,attr"`
+	PremiumRenewalPrice      *float64 `xml:"PremiumRenewalPrice,attr"`
+	PremiumRestorePrice      *float64 `xml:"PremiumRestorePrice,attr"`
+	PremiumTransferPrice     *float64 `xml:"PremiumTransferPrice,attr"`
+	IcannFee                 *float64 `xml:"IcannFee,attr"`
+	EapFee                   *float64 `xml:"EapFee,attr"`
+}
+
+// Check returns availability and pricing information for one or more domains.
+// The Namecheap API accepts up to 50 domains per call.
+//
+// Namecheap doc: https://www.namecheap.com/support/api/methods/domains/check/
+func (ds *DomainsService) Check(domains ...string) (*DomainsCheckCommandResponse, error) {
+	var response DomainsCheckResponse
+
+	params := map[string]string{
+		"Command":    "namecheap.domains.check",
+		"DomainList": strings.Join(domains, ","),
+	}
+
+	_, err := ds.client.DoXML(params, &response)
+	if err != nil {
+		return nil, err
+	}
+	if response.Errors != nil && len(*response.Errors) > 0 {
+		apiErr := (*response.Errors)[0]
+
+		return nil, fmt.Errorf("%s (%s)", *apiErr.Message, *apiErr.Number)
+	}
+
+	return response.CommandResponse, nil
+}
